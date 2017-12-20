325a752fe8b962c4580db6480ed57af8b0f874a6
Teerachai Laothong <digitalthailand@outlook.com>
2017-12-20 22:07:04 +0700

    Your first function: Adopting a pet

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -4,4 +4,13 @@ contract Adoption {
 
     address[16] public adopters;
 
+    // Adopting a pet
+    function adopt(uint petId) public view returns (uint) {
+        require(petId >= 0 && petId <= 15);
+
+        adopters[petId] = msg.sender;
+
+        return petId;
+    }
+
 }
